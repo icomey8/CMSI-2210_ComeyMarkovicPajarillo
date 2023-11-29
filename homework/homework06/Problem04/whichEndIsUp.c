15b9e4ca5134680c3047d61bf59f08bab48621b3
Ian Comey <112445892+icomey8@users.noreply.github.com>
2023-11-29 11:52:20 -0800

    added problem 4
     only able to test on little endian so far

---
+++ homework/homework06/Problem04/whichEndIsUp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int check = 0x543210 ;
+    char *c = (char*)&check;
+    
+    if (*c == 0x10) {
+        printf("\n\nYour machine is Little Endian.\n\n");
+    } else {
+        printf("\n\nYour machine is Big Endian\n\n");
+    }
+
+    return 0;
+
+
+    
+}
